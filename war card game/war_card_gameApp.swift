e6e5ee80424e55c102acc81551ed4c1b4564ce04
hilal <onerhilal0@gmail.com>
2023-10-31 16:45:47 +0300

    Initial Commit

---
+++ war card game/war_card_gameApp.swift
@@ -0,0 +1,18 @@
+//
+//  war_card_gameApp.swift
+//  war card game
+//
+//  Created by Hilal Öner on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct war_card_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
